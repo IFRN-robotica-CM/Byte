b9e406366e21ca408f564874d9a69825a24f5bee
Del Del <tuliosilva416@gmail.com>
2026-07-24 16:44:41 -0300

    implementado a funções paraFrente e parar

--- Seguir_linha_tera/Motor.cpp
+++ Seguir_linha_tera/Motor.cpp
@@ -1,10 +1,11 @@
 #include "Motor.h"
 #include <robo_hardware2.h>
 
-void Motor::seguirEmFrente(){
 
+void Motor::seguirEmFrente(){
+  robo.acionarMotores(VELOCIDADE_FRENTE,-VELOCIDADE_FRENTE);
 }
 
 void Motor::parar(){
-
+  robo.acionarMotores(0,0);
 }
